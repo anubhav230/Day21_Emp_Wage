e9138f7dec9e6383681bd119f200e6b2aba81eaa
anubhav230 <anubhavsingh230@gmail.com>
2020-05-12 13:20:03 +0530

    part time added

--- EmployeeWageOops.java
+++ EmployeeWageOops.java
@@ -1,16 +1,30 @@
 public class EmployeeWageOops {
 	public static void main(String[] args) {
-   	int isPresent = 1;
-	int empRatePrHr = 20;
-	int empHr = 8;
-    double empCheck = Math.floor(Math.random() * 10) % 2;
-    System.out.println(empCheck);
-    if (empCheck == isPresent) {
-		System.out.println("employee is present");
-		int perDaySalary = empRatePrHr * empHr;
-		System.out.println("Employee per day salary = "+perDaySalary);
-	}
-    else
-		System.out.println("absent");
-	}
-}
+		int isFullTime = 1;
+		int isPartTime = 2;
+		int empRatePrHr = 20;
+		int empHr = 0;
+		int perDaySalary = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		System.out.println(empCheck);
+		switch ((int)empCheck)
+		{
+			case 1 :
+				empHr = 8;
+				System.out.println("Full time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			case 2 :
+				empHr = 4;
+				System.out.println("Part time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			default :
+				empHr = 0;
+				perDaySalary = 0;
+				System.out.println("Employee per day salary = "+perDaySalary);
+		}//switch
+	}//main()
+}//class
