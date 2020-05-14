f5c0e95c0408cf90af6295215f17a76636179f65
anubhav230 <anubhavsingh230@gmail.com>
2020-05-14 12:19:16 +0530

    save emp wage for 2 company

--- EmployeeWageOops.java
+++ EmployeeWageOops.java
@@ -1,21 +1,38 @@
-class EmplpyeeSalary {
+class EmployeeWageOops {
         //constant
         public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
 
-        public void empSalary(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth ) {
+		private final String company;
+		private final int empRatePerHr;
+		private final int numberOfWorkingDays;
+		private final int maxHrPerMonth;
+		private int totalEmpWage;
+
+		public EmployeeWageOops(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+			this.company = company;
+			this.empRatePerHr = empRatePerHr;
+			this.numberOfWorkingDays = numberOfWorkingDays;
+			this.maxHrPerMonth = maxHrPerMonth;
+
+		}//constructor
+        
+        public void empSalary() {
                 //variables
 
-				int empHrs = 0;
-				int totalEmpHrs = 0;
-				int totalWorkingDays = 0;
-				while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numberOfWorkingDays) {
-					totalWorkingDays++;
-					double empCheck = Math.floor(Math.random() * 10) % 3;
-					switch ((int)empCheck)
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		//int totalEmpWage = 0;
+		//computation
+		while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numberOfWorkingDays) {
+
+			totalWorkingDays++;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int)empCheck)
                         {
-                                case IS_FULL_TIME :
-                                        empHrs = 8;
+        	                case IS_FULL_TIME :
+                	                empHrs = 8;
                                         break;
                                 case IS_PART_TIME :
                                         empHrs = 4;
@@ -23,21 +40,24 @@ class EmplpyeeSalary {
                                 default :
                                         empHrs = 0;
 
-                        }//switch
-						totalEmpHrs += empHrs;
-						System.out.println(totalWorkingDays+" "+totalEmpHrs);
-				}//while
-
-                        int totalEmpWage = totalEmpHrs * empRatePerHr;
-                        System.out.println(company+" "+totalEmpWage);
-
+			}//switch
+			totalEmpHrs += empHrs;
+			System.out.println(totalWorkingDays+" "+totalEmpHrs);
+		}//while
+        	totalEmpWage = totalEmpHrs * empRatePerHr;
+          
         }//empSalary()
-}
-public class EmployeeWageOops {
+		public String toString() {
+			return "Total employee wage for: " +company+ " is " +totalEmpWage;
+		}
+
 
         public static void main(String[] args) {
-                EmplpyeeSalary empWage = new EmplpyeeSalary();
-                empWage.empSalary("DMart", 20, 20, 100);
-				empWage.empSalary("icici", 50, 20, 100);
+                EmployeeWageOops dMart = new EmployeeWageOops("DMart", 20, 20, 100);
+		EmployeeWageOops icici = new EmployeeWageOops("icici", 50, 20, 100);
+                dMart.empSalary();
+		System.out.println(dMart);
+		icici.empSalary();
+		System.out.println(icici);
         }//main()
-}//mainclass
+}//class
